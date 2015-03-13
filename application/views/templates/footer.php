30e5ce8cb8f73b1f11c82f883b0d65247da1a867
Skypshot <skypshots@gmail.com>
2015-03-13 16:25:36 +0100

    add footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,34 +1,58 @@
 <!---------- FOOTER --------->	
-
+<style>
+a:link {color:white}
+.basPage {width:100%; display:flex; margin:0 auto; width:900px;}
+.basPage img {float:left;padding:10px;}
+.menuBas {float:right;padding:10px;}
+.menuBas span {margin:0 5px;}
+</style>
 <div class="footer">
     <div class="infoSupplementaires">
         <div class="enSavoirPlus bloc_footer">
             <b>EN SAVOIR PLUS</b>
             <ul>
-                <li>- Qui sommes-nous ?</li>
-                <li>- Espace presse</li>
-                <li>- Nous contacter</li>
-                <li>- Nous rejoindre</li>
-                <li>- Mensions legales</li>
-                <li>- CGV</li>
+                <li><a href="#">- Qui sommes-nous ?</a></li>
+                <li><a href="#">- Espace presse</a></li>
+                <li><a href="#">- Nous contacter</a></li>
+                <li><a href="#">- Nous rejoindre</a></li>
+                <li><a href="#">- Mensions legales</a></li>
+                <li><a href="#">- CGV</a></li>
             </ul>
         </div>
         <div class="nosServices bloc_footer">
             <b>NOS SERVICES</b>
             <ul>
-                <li>- Information voyages</li>
-                <li>- Remboursement voyage</li>
-                <li>- Paiement sécurisé</li>
-                <li>- Télécharger notre RIB</li>
-                <li>- FAQ</li>
+                <li><a href="#">- Information voyages</a></li>
+                <li><a href="#">- Remboursement voyage</a></li>
+                <li><a href="#">- Paiement sécurisé</a></li>
+                <li><a href="#">- Télécharger notre RIB</a></li>
+                <li><a href="#">- FAQ</a></li>
             </ul>
         </div>
         <div class="newsletter bloc_footer">
-            <b>S'INSCRIRE A LA NEWSLETTER</b><br/>
-            <input type="text" value="votre e-mail"/><br/>
-            <button>S'INSCRIRE</button>
+			<form action="" method="post">
+				<b>S'INSCRIRE A LA NEWSLETTER</b><br/>
+				<input type="text" value="votre e-mail"/><br/>
+				<button>S'INSCRIRE</button>
+			</form>
         </div>
     </div>
+	<div class="basPage">
+		<div class="reseauxSociaux">
+			<img src="#">
+			<img src="#">
+			<img src="#">
+		</div>
+		<div class="menuBas">
+			<span><a href="#">Qui sommes-nous ?</a></span>
+			 - 
+			 <span><a href="#">CGV</a></span>
+			 - 
+			 <span><a href="#">Mentions legales</a></span>
+			 - 
+			 <span><a href="#">Contact</a></span>
+		</div>
+	</div>
 </div>
 <!---------- / FOOTER --------->	
 
