ece40a5286741d4151a80dd4c80bcfb53b09b23f
Skypshot <skypshots@gmail.com>
2015-03-13 15:55:21 +0100

    css footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,16 +1,5 @@
 <!---------- FOOTER --------->	
-<style>
-	body {width:100%}
-	.footer {text-align:center;width:100%;background-color:#0e1728;color:white;padding:20px auto;}
-	.bloc_footer {text-align:left;width:300px;margin-left:20px;padding: 20px 30px;}
-	.infoSupplementaires {display: inline-flex;margin:0 auto; width:900px;}
-	.newsletter input {width:100%;margin:5px 0px;}
-	.newsletter button {background-color:#0076a3;float:right;}
-	
-	@media screen and (max-width: 900px) {
-		.infoSupplementaires {display:inline-block;width:300px;}
-	}
-</style>
+
 <div class="footer">
     <div class="infoSupplementaires">
         <div class="enSavoirPlus bloc_footer">
