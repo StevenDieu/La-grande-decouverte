8cfc4c81bc12d026d0575199e936c7bced68d4bc
docdokun <groover.Dieu@gmail.com>
2015-03-13 16:14:23 +0100

    bd + conenxion

--- application/controllers/utilisateur.php
+++ application/controllers/utilisateur.php
@@ -9,6 +9,20 @@ class Utilisateur extends CI_Controller {
         $this->load->templateUtilisateur('accueil');
     }
 
+    public function connexion() {
+        $this->load->templateTemplates('accueil');
+    }
+
+    public function validerConnexion() {
+    }
+
+    public function inscription() {
+        $this->load->templateTemplates('accueil');
+    }
+
+    public function validerInscription() {
+    }
+
     public function fiche_voyage_descriptif() {
         $this->load->templateUtilisateur('fiche_voyage_descriptif');
     }
--- application/core/My_loader.php
+++ application/core/My_loader.php
@@ -1,27 +1,35 @@
 <?php
 
 class My_loader extends CI_Loader {
-    public function templateUtilisateur($template_name, $vars = array(), $return = FALSE)
-    {
-        $content  = $this->view('templates/header', $vars, $return);
-        $content .= $this->view('utilisateur/'.$template_name, $vars, $return);
+
+    public function templateUtilisateur($template_name, $vars = array(), $return = FALSE) {
+        $content = $this->view('templates/header', $vars, $return);
+        $content .= $this->view('utilisateur/' . $template_name, $vars, $return);
         $content .= $this->view('templates/footer', $vars, $return);
 
-        if ($return)
-        {
+        if ($return) {
             return $content;
         }
     }
-    
-        public function templateAdministrateur($template_name, $vars = array(), $return = FALSE)
-    {
-        $content  = $this->view('templates/header', $vars, $return);
-        $content .= $this->view('administrateur/'.$template_name, $vars, $return);
+
+    public function templateTemplates($template_name, $vars = array(), $return = FALSE) {
+        $content = $this->view('templates/header', $vars, $return);
+        $content .= $this->view('templates/' . $template_name, $vars, $return);
         $content .= $this->view('templates/footer', $vars, $return);
 
-        if ($return)
-        {
+        if ($return) {
             return $content;
         }
     }
+
+    public function templateAdministrateur($template_name, $vars = array(), $return = FALSE) {
+        $content = $this->view('templates/header', $vars, $return);
+        $content .= $this->view('administrateur/' . $template_name, $vars, $return);
+        $content .= $this->view('templates/footer', $vars, $return);
+
+        if ($return) {
+            return $content;
+        }
+    }
+
 }
