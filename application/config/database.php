3d3718914a7338737f6f5cf12a784abfccf197d2
docdokun <groover.Dieu@gmail.com>
2015-03-18 08:24:45 +0100

    Utilisateur bdd : grandedecouverte

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = 'grandedecouverte';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'lagrandecouverte';
 $db['default']['dbdriver'] = 'mysql';
