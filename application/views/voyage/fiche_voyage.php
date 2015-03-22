aac712958f626a5e18851833824bfffced5c953f
alexandreBoussemart <alex@MBPdealexandre.home>
2015-03-22 22:52:14 +0100

    image fancy

--- application/views/voyage/fiche_voyage.php
+++ application/views/voyage/fiche_voyage.php
@@ -1,5 +1,7 @@
 <!---------- CONTENT --------->	
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZo93gQX7j_kr0Bn3oqfwfIIPCQLAKhuI"></script>
+<script type="text/javascript" src ="<?php echo asset_url(''); ?>librairie/js/fancybox/jquery.fancybox.js?v=2.1.5"></script>
+<link href="<?php echo asset_url(''); ?>librairie/css/fancybox/jquery.fancybox.css?v=2.1.5" type="text/css" rel="stylesheet"/>
     
 <div class="content fiche_voyage">
 	<script type="text/javascript"> 
