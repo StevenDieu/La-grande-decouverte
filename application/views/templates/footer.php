e8bff9b77fe1acb79e98128d337dcf361f309aac
docdokun <groover.Dieu@gmail.com>
2015-03-22 21:23:04 +0100

    suppression ancien footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,56 +1,15 @@
-<!---------- FOOTER --------->	
-<div class="footer">
-    <div class="infoSupplementaires">
-        <div class="enSavoirPlus bloc_footer">
-            <b>EN SAVOIR PLUS</b>
-            <ul>
-                <li><a href="#">- Qui sommes-nous ?</a></li>
-                <li><a href="#">- Espace presse</a></li>
-                <li><a href="#">- Nous contacter</a></li>
-                <li><a href="#">- Nous rejoindre</a></li>
-                <li><a href="#">- Mensions legales</a></li>
-                <li><a href="#">- CGV</a></li>
-            </ul>
+<div id="footer" class="row">
+    <div class="col-md-12"></div>
+    <div class="col-md-12">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-4">.col-md-1</div>
+                <div class="col-md-4">.col-md-1</div>
+                <div class="col-md-4">.col-md-1</div>
+            </div>
         </div>
-        <div class="nosServices bloc_footer">
-            <b>NOS SERVICES</b>
-            <ul>
-                <li><a href="#">- Information voyages</a></li>
-                <li><a href="#">- Remboursement voyage</a></li>
-                <li><a href="#">- Paiement sécurisé</a></li>
-                <li><a href="#">- Télécharger notre RIB</a></li>
-                <li><a href="#">- FAQ</a></li>
-            </ul>
-        </div>
-        <div class="newsletter bloc_footer">
-            <form action="" method="post">
-                <b>S'INSCRIRE A LA NEWSLETTER</b><br/>
-                <input type="text" class="form-control" placeholder="votre e-mail"/>
-                <input type="button" class="btn btn-primary" value="S'INSCRIRE" />
-            </form>
-        </div>
+        <div class="col-md-1"></div>
     </div>
-    <div class="basPage">
-        <div class="reseauxSociaux">
-            <img src="#">
-            <img src="#">
-            <img src="#">
-        </div>
-        <div class="menuBas">
-            <span><a href="#">Qui sommes-nous ?</a></span>
-            - 
-            <span><a href="#">CGV</a></span>
-            - 
-            <span><a href="#">Mentions legales</a></span>
-            - 
-            <span><a href="#">Contact</a></span>
-        </div>
-    </div>
+    <div class="col-md-1"></div>
 </div>
-<!---------- / FOOTER --------->	
-
-<script src="<?php echo asset_url(''); ?>/librairie/js/bootstrap.min.js"></script>
-<script type="text/javascript" src = "<?php echo asset_url(''); ?>js/responsiveslides.min.js" ></script>
-
-</body> 
-</html>
---
+++ application/views/templates/footer2.php
@@ -0,0 +1,54 @@
+<!---------- FOOTER --------->	
+<div class="footer">
+    <div class="infoSupplementaires">
+        <div class="enSavoirPlus bloc_footer">
+            <b>EN SAVOIR PLUS</b>
+            <ul>
+                <li><a href="#">- Qui sommes-nous ?</a></li>
+                <li><a href="#">- Espace presse</a></li>
+                <li><a href="#">- Nous contacter</a></li>
+                <li><a href="#">- Nous rejoindre</a></li>
+                <li><a href="#">- Mensions legales</a></li>
+                <li><a href="#">- CGV</a></li>
+            </ul>
+        </div>
+        <div class="nosServices bloc_footer">
+            <b>NOS SERVICES</b>
+            <ul>
+                <li><a href="#">- Information voyages</a></li>
+                <li><a href="#">- Remboursement voyage</a></li>
+                <li><a href="#">- Paiement sécurisé</a></li>
+                <li><a href="#">- Télécharger notre RIB</a></li>
+                <li><a href="#">- FAQ</a></li>
+            </ul>
+        </div>
+        <div class="newsletter bloc_footer">
+            <form action="" method="post">
+                <b>S'INSCRIRE A LA NEWSLETTER</b><br/>
+                <input type="text" class="form-control" placeholder="votre e-mail"/>
+                <input type="button" class="btn btn-primary" value="S'INSCRIRE" />
+            </form>
+        </div>
+    </div>
+    <div class="basPage">
+        <div class="reseauxSociaux">
+            <img src="#">
+            <img src="#">
+            <img src="#">
+        </div>
+        <div class="menuBas">
+            <span><a href="#">Qui sommes-nous ?</a></span>
+            - 
+            <span><a href="#">CGV</a></span>
+            - 
+            <span><a href="#">Mentions legales</a></span>
+            - 
+            <span><a href="#">Contact</a></span>
+        </div>
+    </div>
+</div>
+<!---------- / FOOTER --------->	
+
+
+</body> 
+</html>
