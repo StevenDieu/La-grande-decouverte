4ed44f74a81c83eb9b6a63242453609caa662ab1
docdokun <groover.Dieu@gmail.com>
2015-03-23 18:59:01 +0100

    footer
    footer

--- application/controllers/voyage/fiche.php
+++ application/controllers/voyage/fiche.php
@@ -11,6 +11,5 @@ class Fiche extends CI_Controller {
     public function index() {
         $data["css"] = "ficheproduit";
         $this->load->templateVoyage('/fiche_voyage', $data);
-        
     }
 }
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,30 +1,148 @@
 <div id="footer" >
     <div class="row blockFooter_1">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
             <div id="footer" class="row">
                 <div class="col-md-4 trait"></div>
                 <div class="col-md-4 tailleTitreContact"><h2 class="titre">CONTACTEZ NOUS</h2></div>
                 <div class="col-md-4 trait"></div>
             </div>
         </div>
-        <div class="col-md-2"></div>
-    </div>
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-            au
-        </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-1"></div>
     </div>
 
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8 numerosFooter">
+    <div class="row blockFooter_2">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            au
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="row blockFooter_3">
+        <div class="col-md-1"></div>
+        <div class="col-md-10 numerosFooter">
             06 71 69 85 16
         </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-1"></div>
     </div>
+
+    <div class="row blockFooter_4">
+        <div class="col-md-1"></div>
+        <div class="col-md-10 explicationFooter">
+            du lundi au vendredi<br/>
+            de 9h à 12h30 et de 13h30 à 17h15<br/>
+            Email : lagrandecouverte@gmail.com<br/>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="row blockFooter_5">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="col-md-4 "></div>
+            <div class="col-md-4 trait"></div>
+            <div class="col-md-4 "></div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="row blockFooter_6">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="col-md-4 blockFooter_6_1">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="titre">INSCRIVEZ-VOUS POUR NOTRE NEWSLETTER</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 textFooter">
+                        <p>Restez à jour avec nos voyages, nos carnets de voyage, nos aventures et des invitations à des événements spéciaux.</p>
+                        <p>Vous pouvez vous retirer à tout moment.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" placeholder="votre e-mail"/>
+                        <input type="button" class="btn btn-primary buttonNewsletter" value="S'INSCRIRE" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 blockFooter_6_2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="titre">RESTEZ CONNECTÉ AVEC LA GRANDE DECOUVERTE</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 textFooter">
+                        <a href="#" target="_blank" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-facebook.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-google.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-linkedin.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-rss.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-tumblr.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-twitter.png" alt=""></a>
+                        <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-youtube.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4 blockFooter_6_3">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="titre">EN SAVOIR PLUS SUR LA GRANDE DECOUVERTE</h3>
+                    </div>
+
+                </div>
+                <div class="row textFooter">
+                    <ul>
+                        <li><a href="#" target="_blank">Qui sommes-nous ?</a></li>
+                        <li><a href="#" target="_blank">Espace presse</a></li>
+                        <li><a href="#" target="_blank">Nous contacter</a></li>
+                        <li><a href="#" target="_blank">Nous rejoindre</a></li>
+                        <li><a href="#" target="_blank">Mensions legales</a></li>
+                        <li><a href="#" target="_blank">CGV</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="row blockFooter_7">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="col-md-4 "></div>
+            <div class="col-md-4 trait"></div>
+            <div class="col-md-4 "></div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="row blockFooter_7">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="menuBas">
+                <span><a href="#">Infos légales</a></span>
+                &nbsp; | &nbsp;
+                <span><a href="#">FAQ</a></span>
+                &nbsp; | &nbsp;
+                <span><a href="#">Conditions générales de vente</a></span>
+                &nbsp; | &nbsp;
+                <span><a href="#">Conditions générales d'utilisation du site </a></span>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+
+
 </div>
 
 
