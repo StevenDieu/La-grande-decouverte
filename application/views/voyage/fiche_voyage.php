cffe8867a100cd2c089c70adb441030cda782f1b
docdokun <groover.Dieu@gmail.com>
2015-03-24 16:12:51 +0100

    fiche voyage

--- application/views/voyage/fiche_voyage.php
+++ application/views/voyage/fiche_voyage.php
@@ -1,3 +1,22 @@
-<div class="content fiche_voyage">
-    
+<!---------- CONTENT ---------->	
+<div class="fiche_voyage">
+    <!-- Slideshow 4 -->
+    <div class="callbacks_container">
+        <ul class="rslides" id="slider_top">
+            <li>
+                <img src="<?php echo asset_url(''); ?>images/1.jpg" alt="">
+                <p class="caption">This is a caption</p>
+            </li>
+            <li>
+                <img src="<?php echo asset_url(''); ?>images/2.jpg" alt="">
+                <p class="caption">This is another caption</p>
+            </li>
+            <li>
+                <img src="<?php echo asset_url(''); ?>images/3.jpg" alt="">
+                <p class="caption">The third caption</p>
+            </li>
+        </ul>
+    </div>
+
+    <div style="clear:both"></div>
 </div>
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,69 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function initialiseResponsiveSilide(id) {
+    jQuery(function () {
+        jQuery(id).responsiveSlides({
+            auto: true,
+            pager: false,
+            timeout: 6000,
+            nav: true,
+            speed: 500,
+            namespace: "callbacks",
+            before: function () {
+                jQuery('.events').append("<li>before event fired.</li>");
+            },
+            after: function () {
+                jQuery('.events').append("<li>after event fired.</li>");
+            }
+        });
+    });
+}
 
 
+
+$(document).ready(function () {
+    initialiseResponsiveSilide('#slider_top');
+
+    $('.onglet_fiche_inner a').click(function (event) {
+        event.preventDefault();
+    });
+
+    $('.onglet_mobile a').click(function (event) {
+        event.preventDefault();
+    });
+
+    $(".onglet_fiche_inner a").click(function () {
+        $('.contenu_onglet .contenu_fiche_onglet').hide();
+        $('.onglet_fiche .onglet_fiche_inner .onglet').removeClass('active');
+        $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+        $('.contenu_onglet .onglet_mobile').removeClass('active');
+
+        $(this).parent().toggleClass('active');
+        $('.contenu_onglet #' + $(this).parent().attr('id')).toggleClass('active');
+        $('.contenu_onglet #' + $(this).parent().attr('id') + "mobile").toggleClass('active');
+        if ($(this).parent().attr('id') == 'onglet2') {
+            $('.contenu_onglet #' + $(this).parent().attr('id')).show();
+            centrage();
+        } else {
+            $('.contenu_onglet #' + $(this).parent().attr('id')).slideDown("slow");
+        }
+    });
+
+    $(".contenu_onglet .onglet_mobile a").click(function () {
+        if ($('.contenu_onglet #' + $(this).parent().attr('id')).hasClass('active')) {
+            $('.contenu_onglet .contenu_fiche_onglet').slideUp("slow");
+            $('.contenu_onglet .onglet_mobile').removeClass('active');
+            $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+            return true;
+        }
+        $('.contenu_onglet .contenu_fiche_onglet').hide();
+        $('.contenu_onglet .onglet_mobile').removeClass('active');
+        $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+
+        $('.contenu_onglet #' + $(this).parent().attr('id')).toggleClass('active');
+        if ($(this).parent().attr('id') == 'onglet2mobile') {
+            $('.contenu_onglet .' + $(this).parent().attr('id')).show();
+            centrage();
+        } else {
+            $('.contenu_onglet .' + $(this).parent().attr('id')).slideDown("slow");
+        }
+    });
+});
