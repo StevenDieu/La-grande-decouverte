2e353e6288e8a6e860a86185cba8b5ee6c4e3668
Skypshot <skypshots@gmail.com>
2015-03-25 10:07:30 +0100

    controller contact

--- application/controllers/contact/index.php
+++ application/controllers/contact/index.php
@@ -4,10 +4,42 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Index extends CI_Controller {
+	
+	function verifContact() {
+        $this->load->library('form_validation');
 
+        $this->form_validation->set_rules('nom', 'nom', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('prenom', 'prenom', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('mail', 'mail', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('objet', 'objet', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('Message', 'Message', 'trim|required|xss_clean');
 
-
-    function index() {
-        echo 'ok';
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->templateUser('page_inscription');
+        } else {
+            $nom = $this->input->post('nom');
+            $prenom = $this->input->post('prenom');
+            $mail = $this->input->post('mail');
+            $objet = $this->input->post('objet');
+            $Message = $this->input->post('Message');
+			
+            envoie_mail($mail,$objet,$Message);
+			
+            redirect('pages/contact', 'refresh');
+        }
     }
+	
+	function envoie_mail($mail,$objet,$message) {
+		$this->load->library('email');
+
+		$this->email->from($mail, $nom." ".$prenom);
+		$this->email->to(this->$mail);
+		
+		$this->email->subject(this->$objet);
+		$this->email->message(this->$message);
+
+		$this->email->send();
+
+		echo $this->email->print_debugger();
+	}
 }
