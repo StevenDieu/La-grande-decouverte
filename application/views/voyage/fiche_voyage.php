f7003fef527928008e644c1789b9372c718831fd
docdokun <groover.Dieu@gmail.com>
2015-03-26 10:12:33 +0100

    fiche voyage

--- application/views/voyage/fiche_voyage.php
+++ application/views/voyage/fiche_voyage.php
@@ -19,6 +19,7 @@
 
     <div style="clear:both"></div>
     <div class="content_fiche_voyage">
+
         <div class="carnet_user">
             <p>Carnet : public</p>
         </div>
@@ -29,22 +30,51 @@
                 <a href="#" target="_blank"><img src="<?php echo asset_url(''); ?>images/footer/img-social-linkedin.png" class="icone-social" alt=""></a>
             </p>
         </div> 
+    </div>
+    <div style="clear:both"></div>
+    <div class="baniere_description">
+        <div class="content_fiche_voyage">
 
-        <div style="clear:both"></div>
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="bloc_image_profile">
-                    <img src="<?php echo asset_url(''); ?>images/utilisateur/01/fabrice-instinct-voyageur.jpg" class="image_profile" alt="Image carnet de voyage"/>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="bloc_image_profile">
+                        <img src="<?php echo asset_url(''); ?>images/utilisateur/01/fabrice-instinct-voyageur.jpg" class="image_profile" alt="Image carnet de voyage"/>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <h1>JOHN DOE</h1>
+                    <p>Je m'appelle John Doe, je suis étudiant sur lIlle. Les sujets qui m'intéressent sont des plus divers, de la société sud américaine et ses transformations contemporaines aux débats politiques nationaux (France) ou internationaux, en passant par la littérature, les questions environnementales,les théroies socialogiques...</P>
                 </div>
             </div>
-            <div class="col-lg-9">
-                <h1>JOHN DOE</h1>
-                <p>Je m'appelle John Doe, je suis étudiant sur lIlle. Les sujets qui m'intéressent sont des plus divers, de la société sud américaine et ses transformations contemporaines aux débats politiques nationaux (France) ou internationaux, en passant par la littérature, les questions environnementales,les théroies socialogiques...</P>
+        </div>
+    </div>
+    <div class="content_fiche_voyage">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="textJour">
+                    Jour 1 : Arrivée à Santiago
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <div class="pointRouge"></div>
+            </div>
+            <div class="col-lg-6">
+                <div class="blockJour">
+                    <div class="descriptionJour">
+                        <div class="textDescriptionJourHaut">
+                            Arrivée à Santiago après 14 heures de vol.<br/>
+                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
+                        </div>
+                        <div class="textDescriptionJourBas">
+                            Lire la fiche
+                        </div>
+                    </div>
+                    <div class="imgJour">
+                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/>
+                    </div>
+                </div>
             </div>
         </div>
-
-
-
-
     </div>
+    <div style="clear:both"></div>
 </div>
