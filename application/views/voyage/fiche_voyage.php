db8f5668f9ecdeed9c21d2ffbc7c0f5134ed46a7
docdokun <groover.Dieu@gmail.com>
2015-03-26 15:25:29 +0100

    fiche voyage fini

--- application/views/voyage/fiche_voyage.php
+++ application/views/voyage/fiche_voyage.php
@@ -67,18 +67,22 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">                        
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                Arrivée à Santiago après 14 heures de vol.<br/>
+                                Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                    
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -93,17 +97,21 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Comprise entre Villarrica et Puerto Montt, cette superbe région peut se prêter à un séjour de plusieurs jours si l’on souhaite pratiquer des sports en pleine nature ou encore s’aventurer à l’assaut du volcan Villarrica ! Pucón est une bonne base pour sillonner la région.	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">                        
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                Comprise entre Villarrica et Puerto Montt, cette superbe région peut se prêter à un séjour de plusieurs jours si l’on souhaite pratiquer des sports en pleine nature ou encore s’aventurer à l’assaut du volcan Villarrica ! Pucón est une bonne base pour sillonner la région.	
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/region_lacs.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                    
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/region_lacs.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -118,17 +126,21 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Ce gros village de 5 000 habitants est construit à plus de 2 400 m d’altitude. Il a gardé son ambiance coloniale avec ses rues pavées qui invitent à la flânerie et ses jolies bâtisses en adobe. En vous baladant dans les rues de la ville, ne maquez pas l’église de San Pedro et ...	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">                       
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                Ce gros village de 5 000 habitants est construit à plus de 2 400 m d’altitude. Il a gardé son ambiance coloniale avec ses rues pavées qui invitent à la flânerie et ses jolies bâtisses en adobe. En vous baladant dans les rues de la ville, ne maquez pas l’église de San Pedro et ...	
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/san_pedro.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                    
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/san_pedro.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -143,17 +155,21 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            La culture de la vigne est de plus en plus développée dans la région centrale du Chili. Le pays est désormais un acteur majeur sur la scène internationale du vin ...
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">        
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                La culture de la vigne est de plus en plus développée dans la région centrale du Chili. Le pays est désormais un acteur majeur sur la scène internationale du vin ...
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/route_vins.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                    
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/route_vins.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -168,17 +184,21 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Pour ceux qui préfèrent la mer, c’est en direction de la belle Valparaiso qu’il faut se rendre. La 2ème ville du pays en nombre d’habitant a su garder le charme du village de pêcheur qu’elle était autrefois. ...
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">                        
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                Pour ceux qui préfèrent la mer, c’est en direction de la belle Valparaiso qu’il faut se rendre. La 2ème ville du pays en nombre d’habitant a su garder le charme du village de pêcheur qu’elle était autrefois. ...
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/valparaiso.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                   
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/valparaiso.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -193,17 +213,21 @@
                 <div class="pointRouge"></div>
             </div>
             <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                             Santiago est l'une de ces grandes villes où plus vous regardez, plus vous en trouvez : les cafés et boîtes de nuit branchés qui pullulent à Bellavista, les collections artistiques - dont regorge le parc Forestal - ...
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
+                <div class="row blockRow">                        
+                    <div class="col-md-8">
+                        <div class="descriptionJour">
+                            <div class="textDescriptionJourHaut">
+                                Santiago est l'une de ces grandes villes où plus vous regardez, plus vous en trouvez : les cafés et boîtes de nuit branchés qui pullulent à Bellavista, les collections artistiques - dont regorge le parc Forestal - ...
+                            </div>
+                            <div class="textDescriptionJourBas">
+                                <a href="#">Lire la fiche</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/visite.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    <div class="col-md-4">                                    
+                        <div class="imgJour">
+                            <img src="<?php echo asset_url(''); ?>images/ficheVoyage/visite.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                        </div>
                     </div>
                 </div>
             </div>
