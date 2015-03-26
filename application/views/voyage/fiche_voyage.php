6efaca35410ebe53c8457bf5894e2af8d5f4edfa
VL10 <vlockwood@efficom-lille.fr>
2015-03-26 13:58:37 +0100

    du travail et encore du travail (bien fait) by Vivi

--- application/views/voyage/fiche_voyage.php
+++ application/views/voyage/fiche_voyage.php
@@ -86,7 +86,7 @@
         <div class="row blocVoyage">
             <div class="col-md-5">
                 <div class="textJour">
-                    Jour 1 : Arrivée à Santiago
+                    Jour 2 : La région des Lacs
                 </div>
             </div>
             <div class="col-md-1">
@@ -96,8 +96,82 @@
                 <div class="blockJour">
                     <div class="descriptionJour">
                         <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
+                            Comprise entre Villarrica et Puerto Montt, cette superbe région peut se prêter à un séjour de plusieurs jours si l’on souhaite pratiquer des sports en pleine nature ou encore s’aventurer à l’assaut du volcan Villarrica ! Pucón est une bonne base pour sillonner la région.	
+                        </div>
+                        <div class="textDescriptionJourBas">
+                            <a href="#">Lire la fiche</a>
+                        </div>
+                    </div>
+                    <div class="imgJour">
+                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row blocVoyage">
+            <div class="col-md-5">
+                <div class="textJour">
+                    Jour 3 : San Pedro de Atacama
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="pointRouge"></div>
+            </div>
+            <div class="col-md-6">
+                <div class="blockJour">
+                    <div class="descriptionJour">
+                        <div class="textDescriptionJourHaut">
+                            Ce gros village de 5 000 habitants est construit à plus de 2 400 m d’altitude. Il a gardé son ambiance coloniale avec ses rues pavées qui invitent à la flânerie et ses jolies bâtisses en adobe. En vous baladant dans les rues de la ville, ne maquez pas l’église de San Pedro et ses murs blancs reflétant la lumière du soleil (qui n’est jamais caché par un quelconque nuage dans cette région).	
+                        </div>
+                        <div class="textDescriptionJourBas">
+                            <a href="#">Lire la fiche</a>
+                        </div>
+                    </div>
+                    <div class="imgJour">
+                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row blocVoyage">
+            <div class="col-md-5">
+                <div class="textJour">
+                    Jour 4&5 : Route des vins et traversée des Andes
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="pointRouge"></div>
+            </div>
+            <div class="col-md-6">
+                <div class="blockJour">
+                    <div class="descriptionJour">
+                        <div class="textDescriptionJourHaut">
+                            La culture de la vigne est de plus en plus développée dans la région centrale du Chili. Le pays est désormais un acteur majeur sur la scène internationale du vin et de nombreuses bodegas ponctuent le paysage.our une excursion au vert, un bol d’air frais et une découverte des vignobles de la région, l’idéal est de suivre la route des vins. L’un des meilleurs itinéraire pour découvrir toute la variété de vignobles est de partir de Santiago en remontant vers le nord...	
+                        </div>
+                        <div class="textDescriptionJourBas">
+                            <a href="#">Lire la fiche</a>
+                        </div>
+                    </div>
+                    <div class="imgJour">
+                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row blocVoyage">
+            <div class="col-md-5">
+                <div class="textJour">
+                    Jour 5 : Valparaiso si j'y suis ! Au Chili
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="pointRouge"></div>
+            </div>
+            <div class="col-md-6">
+                <div class="blockJour">
+                    <div class="descriptionJour">
+                        <div class="textDescriptionJourHaut">
+                            Pour ceux qui préfèrent la mer, c’est en direction de la belle Valparaiso qu’il faut se rendre. La 2ème ville du pays en nombre d’habitant a su garder le charme du village de pêcheur qu’elle était autrefois. Des milliers de maisons multicolores sont accrochées aux flancs des cerros, les collines qui composent la ville, et forment un spectacle vraiment unique et magnifique.
                         </div>
                         <div class="textDescriptionJourBas">
                             <a href="#">Lire la fiche</a>
@@ -138,7 +212,7 @@
         <div class="row blocVoyage">
             <div class="col-md-5">
                 <div class="textJour">
-                    Jour 1 : Arrivée à Santiago
+                    Jour 6&7 : Visite de Santiago et retour Paris
                 </div>
             </div>
             <div class="col-md-1">
@@ -148,86 +222,7 @@
                 <div class="blockJour">
                     <div class="descriptionJour">
                         <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
-                        </div>
-                    </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row blocVoyage">
-            <div class="col-md-5">
-                <div class="textJour">
-                    Jour 1 : Arrivée à Santiago
-                </div>
-            </div>
-            <div class="col-md-1">
-                <div class="pointRouge"></div>
-            </div>
-            <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
-                        </div>
-                    </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row blocVoyage">
-            <div class="col-md-5">
-                <div class="textJour">
-                    Jour 1 : Arrivée à Santiago
-                </div>
-            </div>
-            <div class="col-md-1">
-                <div class="pointRouge"></div>
-            </div>
-            <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
-                        </div>
-                        <div class="textDescriptionJourBas">
-                            <a href="#">Lire la fiche</a>
-                        </div>
-                    </div>
-                    <div class="imgJour">
-                        <img src="<?php echo asset_url(''); ?>images/ficheVoyage/Av-Alameda.jpg" class="image_profile" alt="Image Voyage 1"/> 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row blocVoyage">
-            <div class="col-md-5">
-                <div class="textJour">
-                    Jour 1 : Arrivée à Santiago
-                </div>
-            </div>
-            <div class="col-md-1">
-                <div class="pointRouge"></div>
-            </div>
-            <div class="col-md-6">
-                <div class="blockJour">
-                    <div class="descriptionJour">
-                        <div class="textDescriptionJourHaut">
-                            Arrivée à Santiago après 14 heures de vol.<br/>
-                            Accueil à l'aéroport par votre accompagnateur et transfert à votre hôtel dans le centre de Santiago. Après un premier briefing du guide, temps libre pour découverte individuelle...	
+                             Santiago est l'une de ces grandes villes où plus vous regardez, plus vous en trouvez : les cafés et boîtes de nuit branchés qui pullulent à Bellavista, les collections artistiques - dont regorge le parc Forestal - qui s'étendent de l'art précolombien jusqu'à l'art contemporain et son architecture qui va du XVIe siècle avec l'église San Francisco aux immeubles de...
                         </div>
                         <div class="textDescriptionJourBas">
                             <a href="#">Lire la fiche</a>
