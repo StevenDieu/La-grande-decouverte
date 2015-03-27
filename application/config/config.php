b3be1cf2ebcfdcc6f47afdbc157ef26356cc7ab2
docdokun <groover.Dieu@gmail.com>
2015-03-27 13:50:45 +0100

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/TVAFS-1.0';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
