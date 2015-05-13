f75cac38105ddb529d111c6b4856c8af7811b3c5
Skypshot <skypshots@gmail.com>
2015-05-13 14:47:20 +0200

    Carnet de voyages

--- assets/js/liste_carnet.js
+++ assets/js/liste_carnet.js
@@ -1,7 +1,54 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function  clickContenuOnglet(element) {
+    $('.contenu_onglet .contenu_fiche_onglet').hide();
+    $('.onglet_fiche .onglet_fiche_inner .onglet').removeClass('active');
+    $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+    $('.contenu_onglet .onglet_mobile').removeClass('active');
+
+    $(element).parent().toggleClass('active');
+    $('.contenu_onglet #' + $(element).parent().attr('id')).toggleClass('active');
+    $('.contenu_onglet #' + $(element).parent().attr('id') + "mobile").toggleClass('active');
+    if ($(element).parent().attr('id') == 'onglet2') {
+        $('.contenu_onglet #' + $(element).parent().attr('id')).show();
+        centrage();
+    } else {
+        $('.contenu_onglet #' + $(element).parent().attr('id')).slideDown("slow");
+    }
+}
+
+function  clickContenuOngletMobile(element) {
+    if ($('.contenu_onglet #' + $(element).parent().attr('id')).hasClass('active')) {
+        $('.contenu_onglet .contenu_fiche_onglet').hide();
+        $('.contenu_onglet .onglet_mobile').removeClass('active');
+        $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+        return true;
+    }
+    $('.contenu_onglet .contenu_fiche_onglet').hide();
+    $('.contenu_onglet .onglet_mobile').removeClass('active');
+    $('.contenu_onglet .contenu_fiche_onglet').removeClass('active');
+
+    $('.contenu_onglet #' + $(element).parent().attr('id')).toggleClass('active');
+    if ($(element).parent().attr('id') == 'onglet2mobile') {
+        $('.contenu_onglet .' + $(element).parent().attr('id')).show();
+        centrage();
+    } else {
+        $('.contenu_onglet .' + $(element).parent().attr('id')).show();
+    }
+
+}
 
 
+$( document ).ready(function() {
+    $(".contenu_onglet .onglet_mobile a").click(function () {
+        clickContenuOngletMobile(this);
+    });
+
+    $(".contenu_onglet .onglet_mobile a").click(function () {
+        clickContenuOngletMobile(this);
+    });
+
+    $('.onglet_mobile a').click(function (event) {
+        event.preventDefault();
+    });
+
+    initialiseResponsiveSilide('#slidercarnet1');
+});
