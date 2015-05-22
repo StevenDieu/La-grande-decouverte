dcafe9104877683adeb011d090d43a42e577afe9
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2015-05-22 22:11:21 +0200

    début tunnel

--- application/controllers/checkout/cart.php
+++ application/controllers/checkout/cart.php
@@ -8,6 +8,7 @@ class Cart extends CI_Controller {
 	function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->model('user');
     }
 
     public function onepage() {
@@ -37,5 +38,42 @@ class Cart extends CI_Controller {
 
     public function recap(){
         $this->load->view('checkout/step/recap');
+    }
+
+    public function login(){
+
+        $this->form_validation->set_rules('login', 'login', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('mdp', 'mdp', 'trim|required|xss_clean|callback_check_database_login');
+
+        if ($this->form_validation->run() == FALSE) {
+            $res = array(
+                'retour' => 'error',
+                'message' => 'login ou mot de passe invalide'
+            );
+        } else {
+            $login = $this->input->post('login');
+            $mdp = $this->input->post('mdp');
+
+            $result = $this->user->login($login, $mdp);
+            if ($result != 0) {
+                $sess_array = array(
+                    'id' => $result[0]->id,
+                    'user' => $login
+                );
+                $this->session->set_userdata('logged_in', $sess_array);
+                $res = array(
+                    'retour' => 'connexion',
+                    'message' => "L'utilisateur est connecte"
+                );
+            }else{
+                $res = array(
+                    'retour' => 'error',
+                    'message' => 'login ou mot de passe invalide'
+                );
+            }   
+        }
+
+        echo json_encode($res);
+
     }
 }
--- application/views/checkout/onepage.php
+++ application/views/checkout/onepage.php
@@ -2,6 +2,7 @@
     var urlBilling = '<?php echo base_url('checkout/cart/billing'); ?>';
     var urlPayment = '<?php echo base_url('checkout/cart/payment'); ?>';
     var urlRecap = '<?php echo base_url('checkout/cart/recap'); ?>';
+    var urlLogin = '<?php echo base_url('checkout/cart/login'); ?>';
 </script>
 
 <div class="content">
@@ -13,15 +14,15 @@
                     	<div id="command_left_column"><!--start command_left_column-->
                         	<div class="command_panel"><!--start command_panel-->
                             	<h2 class="open_command first">Identification</h2>
-                                <div class="inside_command_panel">
+                                <div class="inside_command_panel ajaxLogin">
                                 	<div class="identification">
                                     	<div class="identification_left">
                                         	<h3>vous avez déjà un compte ?</h3>
                                             <form action="#">
-                                            	<p><input type="text" value="Votre Email" onClick="if(this.value=='Votre Email')(this.value='')"  onBlur="if(this.value=='')(this.value='Votre Email')" /></p>
-                                                <p><input type="text" value="Votre Mot de passe" onClick="if(this.value=='Votre Mot de passe')(this.value='')"  onBlur="if(this.value=='')(this.value='Votre Mot de passe')" /></p>
+                                            	<p><input id="login" name="login" type="text" value="Votre Email" onClick="if(this.value=='Votre Email')(this.value='')"  onBlur="if(this.value=='')(this.value='Votre Email')" /></p>
+                                                <p><input id="password" name="password" type="text" value="Votre Mot de passe" onClick="if(this.value=='Votre Mot de passe')(this.value='')"  onBlur="if(this.value=='')(this.value='Votre Mot de passe')" /></p>
                                                 <span><a href="#">J’ai perdu mon mot de passe ?</a></span>
-                                                <div class="submit_command"><input type="submit" value="connexion" /></div>
+                                                <div class="submit_command login"><input id="connexion" type="submit" value="connexion" /></div>
                                             </form>
                                         </div>
                                         <div class="identification_left rgt_command">
@@ -71,12 +72,14 @@
 
 <script type="text/javascript">
     var chargement ="<p><img src='<?php echo base_url(''); ?>assets/images/checkout/ajax-loader.gif' alt='loader'></p>";
+    var chargmeentLogin = "<img class='chargement' src='<?php echo base_url(''); ?>assets/images/checkout/ajax-loader.gif' alt='loader'>";
 
     function getBilling(){
         $.ajax({
             url: urlBilling , // ici l'url du controleur de la vue que tu veux faire appeller
-            type: "post",
+            type: "get",
             data: "" ,
+            async: true,
             beforeSend : function (){
                 $(".inside_command_panel.billing").html(chargement)
             },
@@ -119,6 +122,23 @@
             }
         });
     }
+
+    function getLogin(){
+        $.ajax({
+            url: urlRecap , // ici l'url du controleur de la vue que tu veux faire appeller
+            async: true,
+            type: "post",
+            data: "" ,
+            beforeSend : function (){
+                $(".inside_command_panel.recap").html(chargement);
+            },
+            success: function (result) {
+                // Le result qui est déclaré est le code html que le controlleur va te renvoyer donc tu fais :
+                $(".inside_command_panel.recap").html(result) // Pour afficher le contenu de la view
+                $(".inside_command_panel.recap").parent().addClass('charger');
+            }
+        });
+    }
     
     $( document ).ready(function() {
         $( ".openBilling h2" ).click(function() {
@@ -129,6 +149,33 @@
         });
         $( ".openRecap h2" ).click(function() {
           if(!$(this).parent().hasClass( "charger" )) getRecap();
+        });
+
+        $( "#connexion" ).click(function() {
+          $.ajax({
+            url: urlLogin , // ici l'url du controleur de la vue que tu veux faire appeller
+            type: "post",
+            data: {login: $('#login').val(), mdp: $('#password').val()},
+            beforeSend : function (){
+                $(".identification_left form").append(chargmeentLogin);
+            },
+            success: function (result) {
+                data = jQuery.parseJSON(result);
+                if(data.retour == 'connexion'){
+                    $('.identification_left .chargement').remove();
+                    $(".inside_command_panel.ajaxLogin").html('');
+                    $(".inside_command_panel.ajaxLogin").css('display','none');
+                    $(".open_command.first").removeClass('active');
+                    getBilling();
+                }else{
+                    $('.identification_left .chargement').remove();
+                    alert('pas co');
+                }
+                
+            }
+        });
+
+          return false;
         });
     });
 
