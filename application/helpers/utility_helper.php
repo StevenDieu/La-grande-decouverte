545835f4d7d481928c1cbd464b60db9c37602ed9
docdokun <groover.Dieu@gmail.com>
2015-05-26 13:42:58 +0200

    home

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -1,17 +1,71 @@
 <?php
 
-function carnet_long($i) {
-    if ($url !== "") {
-        return base_url() . 'assets/' . $url;
-    }
-    return base_url() . 'assets/';
+function carnet_long($carnetVoyages,$i) {
+    ?>
+
+    <div class="article_first">
+        <div class="image">
+            <div class="callbacks_container carnet">
+                <a href="#">
+                    <ul class="rslides" id="slidercarnet<?php echo $i ?>">
+                        <li>
+                            <div class="image_slide_carnet">
+                                <img src="<?php echo asset_url(''); ?>images/ficheproduit/carnet/tribut.jpg" alt="">
+                            </div>
+                        </li>
+                        <li>
+                            <div class="image_slide_carnet">
+                                <img src="<?php echo asset_url(''); ?>images/ficheproduit/carnet/tribut2.jpg" alt="">
+                            </div>                                                
+                        </li>
+                    </ul>
+                    <a href="#" class="callbacks_nav callbacks1_nav prev">Previous</a>
+                    <a href="#" class="callbacks_nav callbacks1_nav next">Next</a>
+                </a>
+            </div>
+            <a class="slide_carnet1" href="http://localhost/TVAFS-1.0/assets/../fancybox/popup_carnet.php"></a>
+            <div style="clear:both"></div>
+        </div>
+        <div class="partie_droite">
+            <a  href="#" class="titre">aze</a>
+            <div class="date_auteur"><span><?php echo $carnetVoyages[$i]->vTitre; ?></span></div>
+            <div class="texte"><?php echo substr(strip_tags($carnetVoyages[$i]->vAccroche), 0, 550) . '...'; ?></div>
+            <a href="<?php echo base_url('voyage/carnet') . "?id=" . $carnetVoyages[$i]->cvId ?>" class="lire_suite">Voir le carnet ></a>
+        </div>
+        <script type="text/javascript">initialiseResponsiveSilide('#slidercarnet<?php echo $i ?>');</script>
+    </div>
+    <div style='clear:both'></div>
+    <?php
 }
 
-function carnet_court($i) {
-    if ($url !== "") {
-        return base_url() . 'assets/' . $url;
+function carnet_court($carnetVoyages,$i) {
+    ?>
+    <div class="contenu_article_suivant">
+        <div class="un_article <?php if ($i % 2 == 0) echo "left" ?>">
+            <div class="callbacks_container carnet">
+                <a href="#">
+                    <ul class="rslides" id="slidercarnet<?php echo $i ?>">
+                        <li>
+                            <img src="<?php echo asset_url(''); ?>images/ficheproduit/carnet/tribut.jpg" alt="">
+                        </li>
+                        <li>
+                            <img src="<?php echo asset_url(''); ?>images/ficheproduit/carnet/tribut2.jpg" alt="">
+                        </li>
+                    </ul>
+                </a>
+            </div>
+            <div style="clear:both"></div>
+            <a class="titre"><?php echo $carnetVoyages[$i]->cvTitre; ?></a>
+            <div class="date_auteur"><span><?php echo $carnetVoyages[$i]->vTitre; ?></span></div>
+            <div class="texte"><?php echo substr(strip_tags($carnetVoyages[$i]->vAccroche), 0, 550) . '...'; ?></div>
+            <a href="<?php echo base_url('voyage/carnet') . "?id=" . $carnetVoyages[$i]->cvId ?>" class="lire_suite">Voir le carnet ></a>
+            <script type="text/javascript">initialiseResponsiveSilide('#slidercarnet<?php echo $i ?>');</script>
+        </div>
+    </div>
+    <?php
+    if ($i % 2 == 1) {
+        echo "<div style='clear:both'></div>";
     }
-    return base_url() . 'assets/';
 }
 
 function asset_url($url = "") {
