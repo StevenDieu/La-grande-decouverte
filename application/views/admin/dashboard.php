c1138b79b22165bb3f65ef9c10e65667d9077c69
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2015-06-30 11:45:17 +0200

    correction css grid et suppression lien dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -14,22 +14,4 @@
 
     <a href="<?php echo base_url('admin/carnet_voyages/edit'); ?>">editer carnet voyage</a><br>
     <a href="<?php echo base_url('admin/carnet_voyages/liste'); ?>">liste des carnet voyages</a><br><br>
-
-    <a href="<?php echo base_url('admin/continent/add'); ?>">ajouter continent</a><br>
-    <a href="<?php echo base_url('admin/continent/liste'); ?>">liste des continents</a><br>
-
-   <h3>Gestion des newsletters</h3>
-   <a href="<?php echo base_url('admin/newsletters/liste'); ?>">liste des newsletters</a><br>
-
-   <h3>Gestion des actualite</h3>
-    <a href="<?php echo base_url('admin/actualites/add'); ?>">ajouter une actualité</a><br>
-   <a href="<?php echo base_url('admin/actualites/liste'); ?>">liste des actualites</a><br>
-
-   <h3>Gestion des administrateurs</h3>
-    <a href="<?php echo base_url('admin/administrateur/add'); ?>">ajouter un administrateur</a><br>
-   <a href="<?php echo base_url('admin/administrateur/liste'); ?>">liste des administrateurs</a><br>
-
-   <h3>Gestion des clients</h3>
-   <a href="<?php echo base_url('admin/customer/liste'); ?>">liste des clients</a><br>
-
 </div>
