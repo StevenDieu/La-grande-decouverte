7bba3d31cb7d38019432893ee07706e3b344742f
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2015-07-23 13:11:02 +0200

    modif gestion client dans le BO

--- application/controllers/admin/customer.php
+++ application/controllers/admin/customer.php
@@ -22,13 +22,19 @@ class Customer extends CI_Controller {
         $crud->set_subject('Utilisateurs');
         $crud->columns('id','nom','prenom','banni');
         $crud->required_fields('nom','prenom','mail','description');
-        $crud->add_fields('nom','prenom','mail','description','ip','banni','password','cpassword','last_password');
-        $crud->edit_fields('nom','prenom','mail','description','ip','banni','password','cpassword','last_password');
+        $crud->add_fields('nom','prenom','mail','description','banni','password','cpassword');
+        $crud->edit_fields('nom','prenom','mail','description','banni','password','cpassword','last_password');
         $crud->change_field_type('cpassword','password');
         $crud->change_field_type('last_password','password');
         $crud->callback_edit_field('password',array($this,'set_password_input_to_empty'));
         $crud->callback_add_field('password',array($this,'set_password_input_to_empty'));
+        
+        // vérif des champs mot de passe
+        $crud->set_rules('password', 'password', 'callback_check_mdp');
         $crud->callback_before_update(array($this,'encrypt_password_callback'));
+        $crud->callback_before_insert(array($this,'encrypt_password_callback_insert'));
+        
+
         $crud->unique_fields('mail');
         $crud->unset_read();
         $crud->unset_texteditor('description','full_text');
@@ -37,6 +43,50 @@ class Customer extends CI_Controller {
         $crud->display_as('last_password','Ancien mot de passe');
         $output = $crud->render();
         $this->_example_output($output);
+    }
+
+    function check_mdp($mdp) {
+        //si on est en edit
+        if($this->uri->segment(4) == 'update_validation'){
+            //si les 3 sont vides
+            if(empty($this->input->post('password')) && empty($this->input->post('cpassword')) && empty($this->input->post('last_password'))){
+                return true;
+            }else{
+                //si les 3 sont plein
+                if(!empty($this->input->post('password')) && !empty($this->input->post('cpassword')) && !empty($this->input->post('last_password'))){
+                    //on fait le traitement car les 3 sont remplis
+                    $this->load->model('user');
+                    $this->user->setId($this->uri->segment(5));
+                    $this->user->setPassword($this->input->post('last_password'));
+                    if(($this->input->post('password') == $this->input->post('cpassword')) && $this->user->verifPassUser()){
+                        //tous les mots de passe sont bon
+                        return true;
+                    }else{
+                        $this->form_validation->set_message('check_mdp', 'Nouveau mot de passe différent ou ancien mot de passe incorrect.' );
+                        return false;
+                    }
+                }else{
+                    $this->form_validation->set_message('check_mdp', 'Vous devez remplir les 3 champs pour pouvoir modifier le mot de passe' );
+                    return false;
+                }
+            }
+        }else if($this->uri->segment(4) == 'insert_validation'){
+            //si deux sont plein
+            if(!empty($this->input->post('password')) && !empty($this->input->post('cpassword'))){
+                if($this->input->post('password') == $this->input->post('cpassword')){
+                    //tous les mots de passe sont bon
+                    return true;
+                }else{
+                    $this->form_validation->set_message('check_mdp', 'Les mots de passe ne sont pas identiques.');
+                    return false; 
+                }
+            }else{
+                $this->form_validation->set_message('check_mdp', 'Vous devez remplir les 2 champs mot de passe.' );
+                return false;
+            }
+
+        }
+ 
     }
 
     public function _example_output($output = null)
@@ -60,42 +110,36 @@ class Customer extends CI_Controller {
         return "<input type='password' name='password' value='' />";
     }
 
-    function encrypt_password_callback($post_array, $primary_key) {
-        $this->load->library('encrypt');
-        
-        //Encrypt password only if is not empty. Else don't change the password to an empty field
-        if(!empty($post_array['password']) && !empty($post_array['cpassword']) && !empty($post_array['last_password']))
-        {
-            $this->load->model('user');
-            $this->user->setId($primary_key);
-            $this->user->setPassword($post_array['last_password']);
-            if(($post_array['password'] == $post_array['cpassword']) && $this->user->verifPassUser()){
-                $post_array['password'] = MD5($post_array['password']);
-                unset($post_array['cpassword']);
-                unset($post_array['last_password']);
-                //verif de l'email //
-                $this->load->model('user');
+    /*$this->load->model('user');
                 $this->user->setId($primary_key);
                 $this->user->setMail($post_array['mail']);
-                $result = $this->user->check_mail_user();
-                return $post_array; 
-            }  
+                $result = $this->user->check_mail_user();*/
+
+    function encrypt_password_callback($post_array, $primary_key) {        
+        //si tous les champs sont remplis
+        if(!empty($post_array['password']) && !empty($post_array['cpassword']) && !empty($post_array['last_password']))
+        {
+            $post_array['password'] = MD5($post_array['password']);
             unset($post_array['cpassword']);
             unset($post_array['last_password']);
-            unset($post_array['password']);
-            //verif de l'email //
-            $this->load->model('user');
-            $this->user->setId($primary_key);
-            $this->user->setMail($post_array['mail']);
-            $result = $this->user->check_mail_user();
             return $post_array; 
-        }
-        else
-        {
+        }else{
             unset($post_array['password']);
             unset($post_array['cpassword']);
             unset($post_array['last_password']);
             return $post_array; 
         }
     }
+
+    function encrypt_password_callback_insert($post_array) {        
+        //si tous les champs sont remplis
+        if(!empty($post_array['password']) && !empty($post_array['cpassword']))
+        {
+            $post_array['password'] = MD5($post_array['password']);
+            unset($post_array['cpassword']);
+            return $post_array; 
+        }
+    }
+
+
 }
