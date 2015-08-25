677fe9994575ad3deb549c09167fb7a3586872df
alexandreBoussemart <alexandre.boussemart94@gmail.com>
2015-08-25 13:44:14 +0200

    bouton retour au site page paiement

--- application/views/checkout/step/payment/cb.php
+++ application/views/checkout/step/payment/cb.php
@@ -1,1 +1,4 @@
-<div style="width:100%;"><img style="width: 100%;" src='<?php echo base_url(''); ?>assets/images/cb.jpg' alt='cancel'></div>
+<div style="width:100%;">
+	<img style="width: 100%;" src='<?php echo base_url(''); ?>assets/images/cb.jpg' alt='cancel'>
+	<a href="<?php echo base_url('pages') ?>">Retour au site</a>
+</div>
--- application/views/checkout/step/payment/paypal.php
+++ application/views/checkout/step/payment/paypal.php
@@ -1,1 +1,4 @@
-<div style="width:100%;"><img style="width: 100%;" src='<?php echo base_url(''); ?>assets/images/paypal.jpg' alt='cancel'></div>
+<div style="width:100%;">
+	<img style="width: 100%;" src='<?php echo base_url(''); ?>assets/images/paypal.jpg' alt='cancel'>
+	<a href="<?php echo base_url('pages') ?>">Retour au site</a>
+</div>
